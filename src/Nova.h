b4ef0fb806153858760f859b30093917a00842f5
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2005-10-14 16:14:50 +0000

    include pool
    svn path=/trunk/nova/; revision=2519

---
+++ src/Nova.h
@@ -0,0 +1,13 @@
+
+// Lazy way to include everything
+
+#include "NovaInit.h"
+#include "NovaSemaphore.h"
+#include "NovaThread.h"
+#include "NovaTime.h"
+
+#include "NovaClient.h"
+#include "NovaServer.h"
+#include "NovaTelnetClient.h"
+
+
